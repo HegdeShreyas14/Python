b5b6505ab079fc842bc5aca47742409c78bb586b
HegdeShreyas14 <hegdeshreyas1408@gmail.com>
2025-06-01 21:00:12 +0530

    newpat

--- patterns.py
+++ patterns.py
@@ -1,7 +1,7 @@
 
 def patfunc():
     x,y=map(int,input("Enter the rows and columns").split())
-    tt=int(input(("Enter 1 for upward triangle(asterisk), 2 for downward triangle(asterisk), 3 for triangle of numbers ")))
+    tt=int(input(("Enter 1 for upward triangle(asterisk), 2 for downward triangle(asterisk),3 for centered triangle(asterisk), 4 for triangle of numbers ")))
 
     if tt==1:
         for i in range(1,x+1):
@@ -12,6 +12,16 @@ def patfunc():
         for i in range(x,0,-1):
             for j in range(i):
                 print("*",end=' ')
+            print()
+    elif tt==3:
+        for i in range(x):
+            for j in range(x-i):
+                print(" ",end='')
+            for k in range((2*i-1)):
+                if k%2!=0:
+                    print(" ",end='')
+                else:
+                    print("*",end='')
             print()
     else:
         k=1
